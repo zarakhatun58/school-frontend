420cb4bdd4db450ca7ba184048f17f324d4578f5
Jahanara Khatun <zarakhatun58@gmail.com>
2025-09-03 00:08:34 +0530

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",  
+    trailingSlash: true,
   images: {  domains: [
       "localhost", 
       "management-backend-production-2db3.up.railway.app"
