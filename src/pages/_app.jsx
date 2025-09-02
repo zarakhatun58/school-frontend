46434fc627020ab1ee83615e84cb345d4da252bb
Jahanara Khatun <zarakhatun58@gmail.com>
2025-09-02 23:57:40 +0530

    update netli

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,3 +1,4 @@
+"use client";
 import "../styles/globals.css";
 import { AuthProvider } from "../context/AuthContext";
 import Header from "../components/Header";
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,jsx}"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: { extend: {} },
   plugins: [],
 }
