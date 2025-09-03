d85d2a340d031fe227363d6daf9c3da057f76144
Jahanara Khatun <zarakhatun58@gmail.com>
2025-09-03 11:16:21 +0530

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: { extend: {} },
   plugins: [],
